cf85dd58af0c3f092d89c7d9bd8bd23efcde994e
joma74 <dev-mails@gmx.net>
2018-09-09 20:52:30 +0200

    feat: added function to global scope + comments

--- types/global.d.ts
+++ types/global.d.ts
@@ -7,7 +7,8 @@ import clazz1b = require("../clazz1b")
 declare global {
 	namespace NodeJS {
 		interface Global {
-			clazz1bGlobalVar: clazz1b // does not work, just any
+			clazz1bGlobalVar: clazz1b // does not work, just any; not visible from outside
+			getErrorAsString(err: Error): string // not visible from outside
 		}
 	}
 }
