26c4db3a31ae682c236ffce7788fa4a96612f54a
joma74 <dev-mails@gmx.net>
2018-09-26 13:01:48 +0200

    chore(typing): next batch of typings

--- clazz1c.js
+++ clazz1c.js
@@ -1,3 +1,6 @@
+/**
+ * @class
+ */
 function clazz1c(){
 	this.constructorOnly = 0
 	this.constructorUnknown = undefined
@@ -6,6 +9,6 @@ function clazz1c(){
 clazz1c.prototype.method = function() {
 	this.constructorOnly = 3
 	this.constructorUnknown = "plunkbat"
-	}
+}
 
 module.exports=clazz1c
